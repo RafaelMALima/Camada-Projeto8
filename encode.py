f04e204d77fe934d22fa9a5d945ec8d95eed49ba
RafaelMALima <rafaelmal@al.insper.edu.br>
2022-11-10 15:34:28 -0300

    Sinal modulado

--- encode.py
+++ encode.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-from scipy.signal import firwin, lfilter
 from filtro import LPF
 import sounddevice as sd
 import numpy as np
@@ -8,15 +7,15 @@ import sys
 import scipy.io.wavfile as wav
 
 
-signal = wav.read("TheLick.wav")
-
+sample_rate, signal = wav.read("TheLick.wav")
+signal = signal.astype(np.float32)
+print(sample_rate)
 
 #------------------------------------------------
 # Create a FIR filter and apply it to signal.
 #------------------------------------------------
 
 
-sample_rate = 44100
 
 # The Nyquist rate of the signal.
 nyq_rate = sample_rate / 2.
@@ -26,7 +25,7 @@ cutoff_hz = 2200.0
  
 numtaps = 29
 # Use lfilter to filter the signal with the FIR filter
-filtered_signal = LPF(signal, cutoff_hz, sample_rate)
+filtered_signal = LPF(signal[:,0], cutoff_hz, sample_rate)
 
 print(filtered_signal)
 
@@ -38,8 +37,8 @@ sd.play(filtered_signal, sample_rate)
 
 freq_portadora = 14000
 
-t = (0,5,44100*5)
+t = np.linspace(0,5,sample_rate*5)
 sinal_portadora = np.sin(freq_portadora * 2 * np.pi * t)
 
-sinal_modulado = sinal_portadora*signal
+sinal_modulado = sinal_portadora*filtered_signal[:-2205]
 wav.write("sinal_modulado", sample_rate , sinal_portadora)
